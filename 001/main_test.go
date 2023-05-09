bd82407e14805bbf4e0e2cc0bfb00c6ff540164e
srakotest <anoha@bk.ru>
2023-05-09 20:33:18 +0800

    Task "234.  Palindrome Linked List" from list "Challenges for New Users"     add tests, add ChatGpt version

---
+++ 001/main_test.go
@@ -0,0 +1,147 @@
+package main
+
+import "testing"
+
+func TestXIsPalindrome(t *testing.T) {
+	//arrange
+	testTable := []struct {
+		task     string
+		expected bool
+	}{
+		{
+			task:     "1",
+			expected: true,
+		},
+		{
+			task:     "1-2",
+			expected: false,
+		},
+		{
+			task:     "1-2-1",
+			expected: true,
+		},
+		{
+			task:     "1-1-2-1",
+			expected: false,
+		},
+		{
+			task:     "1-2-3-4-5-6",
+			expected: false,
+		},
+		{
+			task:     "1-2-2-1",
+			expected: true,
+		},
+		{
+			task:     "1-1",
+			expected: true,
+		},
+	}
+
+	for _, testcase := range testTable {
+		//act
+		t.Log(testcase.expected, testcase.task)
+		result := xisPalindrome(buildList(testcase.task))
+		//assert
+		if result != testcase.expected {
+			t.Errorf("Incorrect Result. Expect %t, got %t", testcase.expected, result)
+		}
+	}
+
+}
+
+func TestIsPalindrome(t *testing.T) {
+	//arrange
+	testTable := []struct {
+		task     string
+		expected bool
+	}{
+		{
+			task:     "1",
+			expected: true,
+		},
+		{
+			task:     "1-2",
+			expected: false,
+		},
+		{
+			task:     "1-2-1",
+			expected: true,
+		},
+		{
+			task:     "1-1-2-1",
+			expected: false,
+		},
+		{
+			task:     "1-2-3-4-5-6",
+			expected: false,
+		},
+		{
+			task:     "1-2-2-1",
+			expected: true,
+		},
+		{
+			task:     "1-1",
+			expected: true,
+		},
+	}
+
+	for _, testcase := range testTable {
+		//act
+		t.Log(testcase.expected, testcase.task)
+		result := isPalindrome(buildList(testcase.task))
+		//assert
+		if result != testcase.expected {
+			t.Errorf("Incorrect Result. Expect %t, got %t", testcase.expected, result)
+		}
+	}
+
+}
+
+func TestIsPalindromeGpt(t *testing.T) {
+	//arrange
+	testTable := []struct {
+		task     string
+		expected bool
+	}{
+		{
+			task:     "1",
+			expected: true,
+		},
+		{
+			task:     "1-2",
+			expected: false,
+		},
+		{
+			task:     "1-2-1",
+			expected: true,
+		},
+		{
+			task:     "1-1-2-1",
+			expected: false,
+		},
+		{
+			task:     "1-2-3-4-5-6",
+			expected: false,
+		},
+		{
+			task:     "1-2-2-1",
+			expected: true,
+		},
+		{
+			task:     "1-1",
+			expected: true,
+		},
+	}
+
+	for _, testcase := range testTable {
+		//act
+		t.Log(testcase.expected, testcase.task)
+		result := isPalindromeGpt(buildList(testcase.task))
+		//assert
+		if result != testcase.expected {
+			t.Errorf("Incorrect Result. Expect %t, got %t", testcase.expected, result)
+		}
+	}
+
+}
