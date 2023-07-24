9528445d9b9832112fd53f019e38317c04388483
srakotest <103246180+srakotest@users.noreply.github.com>
2023-07-25 00:21:16 +0700

    "605. Can Place Flowers" from list "LeetCode 75"

---
+++ 011/main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCanPlaceFlowers(t *testing.T) {
+	//arrange
+
+	testTable := []struct {
+		task struct {
+			flowerbed    []int
+			extraFlowers int
+		}
+		expected bool
+	}{
+		{
+			task: struct {
+				flowerbed    []int
+				extraFlowers int
+			}{[]int{1, 0, 0, 0, 1},
+				1},
+			expected: true,
+		},
+		{
+			task: struct {
+				flowerbed    []int
+				extraFlowers int
+			}{[]int{1, 0, 0, 0, 1},
+				2},
+			expected: false,
+		},
+		{
+			task: struct {
+				flowerbed    []int
+				extraFlowers int
+			}{[]int{1, 0},
+				0},
+			expected: true,
+		},
+		{
+			task: struct {
+				flowerbed    []int
+				extraFlowers int
+			}{[]int{0},
+				1},
+			expected: true,
+		},
+		{
+			task: struct {
+				flowerbed    []int
+				extraFlowers int
+			}{[]int{0, 0},
+				1},
+			expected: true,
+		},
+		{
+			task: struct {
+				flowerbed    []int
+				extraFlowers int
+			}{[]int{0, 1},
+				1},
+			expected: false,
+		},
+	}
+
+	for _, testcase := range testTable {
+		//act
+		t.Logf("task: %v, %d, expected: %v", testcase.task.flowerbed, testcase.task.extraFlowers, testcase.expected)
+		result := canPlaceFlowers(testcase.task.flowerbed, testcase.task.extraFlowers)
+		//assert
+
+		if result != testcase.expected {
+			t.Errorf("Incorrect Result. Expect %v, got %v", testcase.expected, result)
+		}
+
+	}
+
+}
